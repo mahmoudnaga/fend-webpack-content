f23df882c66293f4038b2312af44c85813909f24
mahmoudnaga <mahmoudaboelnaga@yahoo.com>
2021-04-04 14:53:23 +0200

    yes

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,8 +1,11 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 
 module.exports = {
+    mode:"development",
     entry: './src/client/index.js',
     devtool: 'source-map',
     module: {
@@ -18,6 +21,14 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
+        }),
+        new CleanWebpackPlugin({
+            dry: true,
+            // Write Logs to Console
+            verbose: true,
+            // Automatically remove all unused webpack assets on rebuild
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false
         })
     ]
 }
